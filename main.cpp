8079fffbf495317f7e7dc94b043e4d4f339b871e
Vy Quoc Huy <huy2004abc@gmail.com>
2024-11-11 22:10:04 +0700

    200 first  primes

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 #include <vector>
 #include "BigInt.h"
 
-// 100 số nguyên tố đầu tiên
+// 200 số nguyên tố đầu tiên
 std::vector<int> first_prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
                                 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 
                                 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 
                                 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 
                                 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 
                                 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 
-                                479, 487, 491, 499, 503, 509, 521, 523, 541};
+                                479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571,
+                                577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653,
+                                659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751,
+                                757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853,
+                                857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947,   
+                                953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033,
+                                1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109,};
 
 // A: Triển khai hàm lũy thừa mô-đun (Exponentiation by Squaring)
 BigInt modular_exponentiation(BigInt base, BigInt exponent, BigInt mod) {
@@ -66,7 +72,6 @@ bool miller_rabin(BigInt n, int k = 5) {
 BigInt generate_safe_prime(int bit_size) {
     BigInt min = BigInt(1) << (bit_size - 1);
     BigInt max = (BigInt(1) << bit_size) - 1;
-
     BigInt prime = 0;
 
     std::random_device rd;
@@ -79,14 +84,17 @@ BigInt generate_safe_prime(int bit_size) {
             prime = prime * 10 + dis(gen);
         }
         BigInt prime2 = (prime - 1) / 2;
+        
         // kiểm tra số nguyên tố có kết thúc bằng 1, 3, 7, 9
-        if (prime.lastDigit() != 1  && prime.lastDigit() != 3 && prime.lastDigit() != 7 && prime.lastDigit() != 9) continue;
-        if (prime2.lastDigit() != 1  && prime2.lastDigit() != 3 && prime2.lastDigit() != 7 && prime2.lastDigit() != 9) continue;
+        if (prime.lastDigit() != 1  && prime.lastDigit() != 3 && prime.lastDigit() != 7 && prime.lastDigit() != 9) 
+            continue;
+        if (prime2.lastDigit() != 1  && prime2.lastDigit() != 3 && prime2.lastDigit() != 7 && prime2.lastDigit() != 9) 
+            continue;
 
         // kiểm tra số nguyên tố có dạng 3k+1
         if (prime % 3 == 1 || prime2 % 3 == 1) continue;
 
-        // chia cho 100 số nguyên tố đầu tiên
+        // chia cho 200 số nguyên tố đầu tiên
         bool is_composite = false;
         for (int p : first_prime) {
             if ((prime % p == 0 && prime != p) || (prime2 % p == 0 && prime2 != p)) {
@@ -122,7 +130,7 @@ BigInt generate_private_key(BigInt p) {
 int main() {
     std::cout << "Diffie-Hellman Key Exchange\n";
     // 1. Sinh số nguyên tố lớn p và phần tử sinh g
-    int bit_size = 512; // Kích thước bit ví dụ, có thể điều chỉnh
+    int bit_size = 128; // Kích thước bit ví dụ, có thể điều chỉnh
 
     time_t start, end;
     start = time(NULL);
